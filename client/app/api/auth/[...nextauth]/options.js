7c35f1a2a1a0663ae41eed0d87c7abf0bf0c7906
szymonzawrotny <szymonzawrotny@gmail.com>
2024-12-02 17:49:16 +0100

    again

--- client/app/api/auth/[...nextauth]/options.js
+++ client/app/api/auth/[...nextauth]/options.js
@@ -56,7 +56,6 @@ export const options = {
         })
     ],
     pages: {
-        signIn: "/",  // Możesz dostosować stronę logowania
-        signOut: "/userpanel"
+        signIn: "/",
     }
 };

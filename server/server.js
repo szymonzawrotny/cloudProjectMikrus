4c4aa9082d2f3491f1ce2c3ad0bf3598f7277d26
szymonzawrotny <szymonzawrotny@gmail.com>
2024-12-02 22:39:20 +0100

    next2

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ let rents = db.collection("rents");
 let users = db.collection("users")
 
 app.get("/",(req,res)=>{
-    res.send("dzień dobry test");
+    res.send("dzień dobry");
 })
 
 app.get("/moviesApi", async (req, res) => {
